a917967a81d6e07a4c49a1963ef5eefb48cac9cf
Marcelo Dias <marcelodkarmo@gmail.com>
2022-08-12 16:48:20 -0300

    Fix: change on axios configuration

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,7 @@
 import axios from 'axios'
 
-type ApiResponse = {
-    token: string,
-    expires_in: string
-}
-
 const api = axios.create({
     baseURL: 'https://my-grandson.herokuapp.com/'
 })
 
-export { api, ApiResponse}
+export default api
